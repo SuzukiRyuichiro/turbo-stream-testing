09626999e037159620f049ca6e2b2884768110e0
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-03-17 13:46:17 +0200

    jey

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,4 @@ Blog.destroy_all
 
 5.times do
   blog = Blog.create(title: Faker::Book.title, content: Faker::Quote.matz)
-  3.times do
-    Comment.create(content: Faker::Quote.matz, blog:)
-  end
 end
